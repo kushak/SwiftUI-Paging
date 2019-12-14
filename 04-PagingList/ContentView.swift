31eaee5c0c2aebbe55cf417b87110f53fd0f7c46
oShipulin <shipulin@restream.rt.ru>
2019-12-14 19:25:55 +0300

    Initial Commit

---
+++ 04-PagingList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  04-PagingList
+//
+//  Created by Oleg Shipulin on 14.12.2019.
+//  Copyright © 2019 Oleg Shipulin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
